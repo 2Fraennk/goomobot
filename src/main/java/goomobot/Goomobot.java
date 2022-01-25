0306982c1504849980431f9b2792fb70feaacdad
fabi <frank.birkhofen@t-systems.com>
2022-01-25 12:17:55 +0100

    add lastrun-switch and message-io-file

--- src/main/java/goomobot/Goomobot.java
+++ src/main/java/goomobot/Goomobot.java
@@ -1,7 +1,7 @@
 package goomobot;
 
 import com.google.gson.*;
-import java.io.IOException;
+import java.io.*;
 import java.net.ProxySelector;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,6 +11,8 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Properties;
+import java.util.Random;
 import static java.net.http.HttpRequest.*;
 
 
@@ -20,8 +22,8 @@ public class Goomobot {
     private static String passwd = "";
     private static String userId = System.getProperty("userId");
     private static String authToken = System.getProperty("authToken");
-    private static String searchMessage = "moin";
-    private static String sendMessage = "Moin, moin!";
+    private static String searchMessage = "";
+    private static String sendMessage = "";
     private static JsonObject responseData = null;
     private static String roomId = System.getProperty("roomId");
     private static String aka = System.getProperty("aka");
@@ -30,19 +32,28 @@ public class Goomobot {
     private static boolean foundMessages = false;
 
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
-        //responseBody = loginAndGetMessages(url,user,passwd);
-        //authToken = getAuthToken(responseBody);
-        //userId = getUserId(responseBody);
-        System.out.println("userId: "+userId);
-        System.out.println("authToken: "+authToken);
-        System.out.println("roomId: "+roomId);
-        System.out.println("aka: "+aka);
-        System.out.println("host: "+host);
-        foundMessages = searchMessages(userId,authToken,roomId,searchMessage);
+
         System.out.println("foundMessages: " + foundMessages);
-        if (foundMessages == true) {
-            sendMessage(userId, authToken, roomId, sendMessage);
-            System.out.println("Passt! Message wurde versendet!");
+
+        String messagesFile = "./messages.txt";
+        InputStream input = new FileInputStream(messagesFile);
+        BufferedReader br = new BufferedReader(new InputStreamReader(input));
+
+        // determine line numbers
+        LineNumberReader lr = new LineNumberReader(new FileReader(messagesFile));
+        int lineNr = 0;
+        while ((lr.readLine()) != null) {
+            lineNr = lr.getLineNumber();
+        }
+
+        for (int i=0; i < lineNr; i++) {
+            searchMessage = br.readLine();
+            System.out.println(searchMessage);
+            foundMessages = searchMessages(userId, authToken, roomId, searchMessage);
+            if (foundMessages == true) {
+                sendMessage(userId, authToken, roomId);
+                //break;
+            }
         }
     }
 
@@ -52,7 +63,7 @@ public class Goomobot {
         URI = new URI(
                 "https",
                 host,
-                "/api/v1chat.search?roomId=blabla=\"DashieristeinkrasserTest2!\"",
+                "/api/v1chat.search?roomId="+roomId+"=\""+sendMessage+"\"",
                 null);
 
         HttpRequest request = newBuilder()
@@ -71,10 +82,8 @@ public class Goomobot {
 
         if (loginResponseCode == 200) {
             return responseData.toString();
-        }
-        else throw new IOException("The login failed with a result of: " + loginResponseCode
+        } else throw new IOException("The login failed with a result of: " + loginResponseCode
                 + " (" + responseData + ")");
-
     }
 
 
@@ -93,17 +102,11 @@ public class Goomobot {
     }
 
     public static boolean searchMessages(String XUserId, String XAuthToken, String roomId, String searchMessage) throws IOException, InterruptedException, URISyntaxException {
-        System.out.println("userId2: "+userId);
-        System.out.println("authToken2: "+authToken);
-        System.out.println("roomId2: "+roomId);
-        System.out.println("aka2: "+aka);
-        System.out.println("host2: "+host);
-
         URI URI;
         URI = new URI(
                 "https",
                 host,
-                "/api/v1/chat.search?roomId="+roomId+"&searchText=\""+searchMessage+"\"",
+                "/api/v1/chat.search?roomId=" + roomId + "&searchText=\""+searchMessage+"\"",
                 null);
 
         HttpRequest request = newBuilder()
@@ -122,57 +125,104 @@ public class Goomobot {
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
         responseData = (JsonObject) JsonParser.parseString(response.body());
-        System.out.println("responseData: "+ responseData.toString());
+        System.out.println("responseData: " + responseData.toString());
         JsonArray responseMessages = responseData.get("messages").getAsJsonArray();
-        for (int i=0;i<responseMessages.size();i++) {
+        for (int i = 0; i < responseMessages.size(); i++) {
             JsonObject message = responseMessages.get(i).getAsJsonObject();
             JsonElement id = message.get("_id");
             JsonElement score = message.get("score");
             JsonElement ts = message.get("ts");
 
-            System.out.println(""+id.toString()+", "+score.toString()+"");
+            System.out.println("" + id.toString() + ", " + score.toString() + "");
 
-            if (score.getAsFloat()>1.0) {
+            if (score.getAsFloat() > 1.0) {
                 String tsString = ts.toString().split("T")[0].split("\"")[1];
                 LocalDate ld = LocalDate.now();
                 if (tsString.equals(ld.toString())) {
                     System.out.println(message.toString());
-                    foundMessages=true;
+                    foundMessages = true;
                 }
             }
         }
-
         return foundMessages;
-
-
-    }
-    
-
-    public static void sendMessage(String XUserId, String XAuthToken, String roomId, String sendMessage) throws IOException, InterruptedException, URISyntaxException {
-        URI URI;
-        URI = new URI(
-                "https",
-                host,
-                "/api/v1/chat.postMessage",
-                null);
-
-        HttpRequest request = newBuilder()
-                .uri(URI)
-                .setHeader("Content-Type", "application/json")
-                .setHeader("X-Auth-Token", XAuthToken)
-                .setHeader("X-User-Id", XUserId)
-                .POST(HttpRequest.BodyPublishers.ofString("{\"alias\": \""+aka+"\",\"channel\": \""+roomId+"\",\"text\": \""+sendMessage+"\"}"))
-                .timeout(Duration.of(30, ChronoUnit.SECONDS))
-                .build();
-
-        HttpResponse<String> response = HttpClient
-                .newBuilder()
-                //.proxy(ProxySelector.getDefault())
-                .build()
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        System.out.println(response.statusCode());
     }
 
 
+    public static void sendMessage(String XUserId, String XAuthToken, String roomId) throws IOException, InterruptedException, URISyntaxException {
+        String propsFile = "./.properties";
+        InputStream input = new FileInputStream(propsFile);
+        sendMessage = chooseSendMessage();
+
+        Properties props = new Properties();
+        props.load(input);
+        String lastSuccessfulMessageCommitDate = props.getProperty("lastSuccessfulMessageCommitDate");
+        System.out.println("lastSuccessfulMessageCommitDate: " + lastSuccessfulMessageCommitDate);
+        input.close();
+
+        if (!lastSuccessfulMessageCommitDate.contains(LocalDate.now().toString())) {
+            URI URI;
+            URI = new URI(
+                    "https",
+                    host,
+                    "/api/v1/chat.postMessage",
+                    null);
+
+            HttpRequest request = newBuilder()
+                    .uri(URI)
+                    .setHeader("Content-Type", "application/json")
+                    .setHeader("X-Auth-Token", XAuthToken)
+                    .setHeader("X-User-Id", XUserId)
+                    .POST(HttpRequest.BodyPublishers.ofString("{\"alias\": \"" + aka + "\",\"channel\": \"" + roomId + "\",\"text\": \"" + sendMessage + "\"}"))
+                    .timeout(Duration.of(30, ChronoUnit.SECONDS))
+                    .build();
+
+            HttpResponse<String> response = HttpClient
+                    .newBuilder()
+                    //.proxy(ProxySelector.getDefault())
+                    .build()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.statusCode());
+
+            if (response.statusCode() == 200) {
+                LocalDate ld;
+                ld = LocalDate.now();
+                System.out.println("ld: " + ld);
+
+                props.setProperty("lastSuccessfulMessageCommitDate", String.valueOf(ld));
+                props.store(new FileOutputStream(propsFile), null);
+
+                System.out.println("message successfully send.");
+            }
+        }
+    }
+
+    private static String chooseSendMessage() throws IOException {
+        String messagesFile = "./messages.txt";
+        InputStream input = new FileInputStream(messagesFile);
+        BufferedReader br = new BufferedReader(new InputStreamReader(input));
+
+        String message2send = null;
+        Random rand = new Random();
+
+        // determine line numbers
+        LineNumberReader lr = new LineNumberReader(new FileReader(messagesFile));
+        int lineNr = 0;
+        while ((lr.readLine()) != null) {
+            lineNr = lr.getLineNumber();
+        }
+
+        // limit the random numbers
+        int upperbound = lineNr;
+        int int_random = rand.nextInt(upperbound);
+
+        // find line with random number
+        for (int i = 0; i < int_random; i++) {
+            message2send = br.readLine();
+            System.out.println(message2send);
+        }
+
+        input.close();
+        return message2send;
+    }
 }
